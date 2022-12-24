4403ceb13c801040145d4bbb15685de3ca599d1e
TimWolfskin <timwolfskin013@gmail.com>
2022-12-24 11:04:16 -0800

    added register page, added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 //import Home from './pages/Home'
 //import ProductList from './pages/ProductList'
-import Product from './pages/Product'
+//import Product from './pages/Product'
+import Register from './pages/Register'
 
 const App = () => {
-  return <Product />
+  return <Register />
 }
 
 export default App
