31dbb7608f544e6f89e8b2af5f7dffd69f661e44
TimWolfskin <timwolfskin013@gmail.com>
2022-12-25 00:20:26 -0800

    added Cart page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 //import Home from './pages/Home'
 //import ProductList from './pages/ProductList'
 //import Product from './pages/Product'
-import Register from './pages/Register'
+//import Register from './pages/Register'
+import Cart from './pages/Cart'
 
 const App = () => {
-  return <Register />
+  return <Cart />
 }
 
 export default App
